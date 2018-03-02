de00152ccbd07986c1f7bd2266e4c0154748d67c
StewBeans <harrisonbsv@gmail.com>
2018-03-02 12:36:19 -0500

    Replaced toString with TreeMap
    Completley removed the 'toString(int)' method and added myMap().

--- src/com/temperatureControl/handler/UnitHandler.java
+++ src/com/temperatureControl/handler/UnitHandler.java
@@ -2,7 +2,10 @@ package com.temperatureControl.handler;
 
 import com.temperatureControl.handler.units.*;
 
+import java.util.TreeMap;
+
 public class UnitHandler {
+
     private final int CELSIUS = 0;
     private final int KELVIN = 1;
     private final int FAHRENHEIT = 2;
@@ -14,13 +17,13 @@ public class UnitHandler {
     /**
      * Converts specified unit to specified unit
      *
-     * @param toUnit unit of measure to convert to
-     * @param fromUnit unit of measure to convert from
+     * @param toUnit      unit of measure to convert to
+     * @param fromUnit    unit of measure to convert from
      * @param temperature value of unit to be converted
      * @return converted temperature
      */
-    public double convert(int toUnit, int fromUnit, double temperature){
-        switch(toUnit){
+    public double convert(int toUnit, int fromUnit, double temperature) {
+        switch (toUnit) {
             case CELSIUS:
                 Celsius celsius = new Celsius();
                 return fromUnit == KELVIN ? celsius.ktoC(temperature) : celsius.ftoC(temperature);
@@ -34,16 +37,32 @@ public class UnitHandler {
         return 0.0;
     }
 
-    public String[] toString(int toUnit){
-        if (toUnit == CELSIUS) {
-            return new String[]{"1: " + KEL, "2: " + FAH};
-        }
-        if (toUnit == KELVIN) {
-            return new String[]{"0: " + CEL, "2: " + FAH};
-        }
-        if (toUnit == FAHRENHEIT) {
-            return new String[]{"0: " + CEL, "1: " + KEL};
-        }
-        return new String[]{"ERROR"};
+    /**
+     * Sets up TreeMap for specified values
+     *
+     * @return the inputted values
+     */
+    public TreeMap<Integer, TreeMap<Integer, String>> myMap() {
+
+        TreeMap<Integer, TreeMap<Integer, String>> treeMap = new TreeMap<Integer, TreeMap<Integer, String>>();
+
+        TreeMap<Integer, String> celMap = new TreeMap<Integer, String>();
+        TreeMap<Integer, String> kelMap = new TreeMap<Integer, String>();
+        TreeMap<Integer, String> fahMap = new TreeMap<Integer, String>();
+
+        celMap.put(1, KEL);
+        celMap.put(2, FAH);
+
+        kelMap.put(0, CEL);
+        kelMap.put(2, FAH);
+
+        fahMap.put(0, CEL);
+        fahMap.put(1, KEL);
+
+        treeMap.put(CELSIUS, celMap);
+        treeMap.put(KELVIN, kelMap);
+        treeMap.put(FAHRENHEIT, fahMap);
+
+        return treeMap;
     }
 }
