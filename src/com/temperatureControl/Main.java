7b4021060a13f93fcb5dffefb5f1ea16c7125db0
StewBeans <harrisonbsv@gmail.com>
2018-03-02 12:40:02 -0500

    R/R
    Removed and replaced all 'toString(int)' methods with myMap()

--- src/com/temperatureControl/Main.java
+++ src/com/temperatureControl/Main.java
@@ -14,29 +14,32 @@ public class Main {
     private static double degrees;
 
     public static void main(String[] args) {
-        int type = 0, tmpNum;
-        char var2;
+        int firstUnitType = 0, secondUnitType, tmpNum;
 
-        while (type != -1) {
+        while (firstUnitType != -1) {
             //Starts conversation with user and asks for their input
             System.out.println("\nWhat unit of temperature will you be inputting?");
             System.out.print("0: Celsius 1: Kelvin 2: Fahrenheit(-1: Exit): ");
-            type = scanner.nextInt();
+            firstUnitType = scanner.nextInt();
             //Check if user input is between specified values
-            if (type != -1 && type >= 0 && type <= 2) {
+            if (firstUnitType != -1 && firstUnitType >= 0 && firstUnitType <= 2) {
+                int firstKey = handler.myMap().get(firstUnitType).firstKey();
+                int lastKey = handler.myMap().get(firstUnitType).lastKey();
                 //Asks user the second question and asks for their input
                 System.out.println("\nWhat would you like to convert to?");
-                System.out.print(handler.toString(type)[0] + " " + handler.toString(type)[1] + ": ");
-                var2 = scanner.next().charAt(0);
+                System.out.print(firstKey + ": " + handler.myMap().get(firstUnitType).get(firstKey) + " "
+                        + lastKey + ": " + handler.myMap().get(firstUnitType).get(lastKey) + ": ");
+                secondUnitType = scanner.nextInt();
                 //Check if user input is between specified value
-                if( var2 == '0' || var2 == '1' || var2 == '2') {
-                    System.out.print("\nEnter the degrees in " + unit[type] + ": ");
+                System.out.print("\nEnter the degrees in " + unit[firstUnitType] + ": ");
+                if (secondUnitType >= 1 && secondUnitType <= 3) {
                     degrees = scanner.nextDouble();
-                    tmpNum = var2 == handler.toString(type)[0].charAt(0) ? 0 : 1;
-                    System.out.printf(degrees + " " + unit[type] + " is: %.2f "
-                            + handler.toString(type)[tmpNum].substring(3),
-                            handler.convert(Integer.parseInt(String.valueOf(
-                                    handler.toString(type)[tmpNum].charAt(0))), type, degrees));
+                    tmpNum = secondUnitType == firstKey ? 0 : 1;
+                    double converted = handler.convert(handler.myMap()
+                            .get(firstUnitType).keySet().toArray()[1].hashCode(), firstUnitType, degrees);
+                    Object convertedName = handler.myMap().get(firstUnitType).values().toArray()[tmpNum];
+                    System.out.printf(degrees + " " + unit[firstUnitType] + " is: %.2f "
+                            + convertedName + "\n", converted);
                 }
             } else {
                 System.out.println("\nInvalid input, please enter a different number.");
