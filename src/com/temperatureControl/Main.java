5dbb8f2eaa9a1ffca2938d7a2c6e88f08470cafd
StewBeans <harrisonbsv@gmail.com>
2018-03-02 14:25:33 -0500

    Bug Fix
    Fixed bug in 'secondUnitType' if statement and the double 'converted'.

--- src/com/temperatureControl/Main.java
+++ src/com/temperatureControl/Main.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
-    private static UnitHandler handler = new UnitHandler();
+    private static Scanner scanner = new Scanner(System.in);    //used to collect user input
+    private static UnitHandler handler = new UnitHandler();     //object of handler class
 
     private static final String[] unit = {"Celsius", "Kelvin", "Fahrenheit"};
 
-    private static double degrees;
+    private static double degrees;  //user inputted double value of degrees to convert
 
     public static void main(String[] args) {
-        int firstUnitType = 0, secondUnitType, tmpNum;
+        int firstUnitType = 0;  //is unit celsius, kelvin or fahrenheit?
+        int secondUnitType;     //is unit celsius, kelvin or fahrenheit?
+        int tmpNum;             //used to determine if secondUnitType == firstKey
 
         while (firstUnitType != -1) {
             //Starts conversation with user and asks for their input
             System.out.println("\nWhat unit of temperature will you be inputting?");
             System.out.print("0: Celsius 1: Kelvin 2: Fahrenheit(-1: Exit): ");
-            firstUnitType = scanner.nextInt();
-            //Check if user input is between specified values
+            firstUnitType = scanner.nextInt();  //Get user input
+            //Validate input is not -1 and is between 0 and 2
             if (firstUnitType != -1 && firstUnitType >= 0 && firstUnitType <= 2) {
                 int firstKey = handler.myMap().get(firstUnitType).firstKey();
                 int lastKey = handler.myMap().get(firstUnitType).lastKey();
@@ -29,14 +31,17 @@ public class Main {
                 System.out.println("\nWhat would you like to convert to?");
                 System.out.print(firstKey + ": " + handler.myMap().get(firstUnitType).get(firstKey) + " "
                         + lastKey + ": " + handler.myMap().get(firstUnitType).get(lastKey) + ": ");
-                secondUnitType = scanner.nextInt();
-                //Check if user input is between specified value
-                System.out.print("\nEnter the degrees in " + unit[firstUnitType] + ": ");
-                if (secondUnitType >= 1 && secondUnitType <= 3) {
-                    degrees = scanner.nextDouble();
-                    tmpNum = secondUnitType == firstKey ? 0 : 1;
-                    double converted = handler.convert(handler.myMap()
-                            .get(firstUnitType).keySet().toArray()[1].hashCode(), firstUnitType, degrees);
+                secondUnitType = scanner.nextInt(); //Get user input
+                //Check if user input is between 0 and 2
+                if (secondUnitType >= 0 && secondUnitType <= 2) {
+                    System.out.print("\nEnter the degrees in " + unit[firstUnitType] + ": ");
+                    degrees = scanner.nextDouble(); //Get user input
+                    tmpNum = secondUnitType == firstKey ? 0 : 1;    //Compares secondUnitType and firstKey
+                    //convert double from either first or last 
+                    //key of myMap()'s key of 'firstUnitType'
+                    double converted = handler.convert(handler.myMap().get(firstUnitType)
+                            .keySet().toArray()[tmpNum].hashCode(), firstUnitType, degrees);
+                    //Get name of converted to unit
                     Object convertedName = handler.myMap().get(firstUnitType).values().toArray()[tmpNum];
                     System.out.printf(degrees + " " + unit[firstUnitType] + " is: %.2f "
                             + convertedName + "\n", converted);
